81aa38fd5603117b06205a311dca125d0a4b8ee1
JoaoRossii <joao.rossi@sptech.school>
2024-05-31 22:42:55 -0300

    insert da pontuacao do quiz no banco de dados pela api

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -1,5 +1,5 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
+// var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -84,7 +84,40 @@ function cadastrar(req, res) {
     }
 }
 
+function insert(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var respostasC = req.body.respostasCorretasServer;
+    var respostasI = req.body.respostasIncorretasServer;
+    var id = req.body.idUsuarioServer;
+
+    if (respostasC == undefined) {
+        res.status(400).send("Sua resposta está undefined!");
+    } else if (respostasI == undefined) {
+        res.status(400).send("Sua resposta está undefined!");
+    } else if (id == undefined) {
+        res.status(400).send("Seu id está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.insert(respostasC, respostasI, id)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    insert
 }
--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -22,7 +22,22 @@ function cadastrar(ra, nome, email, senha) {
     return database.executar(instrucaoSql);
 }
 
+function insert(respostasC, respostasI, id) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", respostasC, respostasI, id);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO quiz (certas, erradas, fkUsuario) VALUES ('${respostasC}', '${respostasI}', '${id}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    insert
 };
